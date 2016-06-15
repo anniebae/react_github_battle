cdc1daf7b1dc67227b891d4beb5582cb9fd3dba0
Annie Bae <bae.annie@gmail.com>
2016-06-15 17:42:29 -0400

    routes rendering router component

--- app/config/routes.js
+++ app/config/routes.js
@@ -10,8 +10,9 @@ var Home = require('../components/Home');
 var routes = (
 	<Router>
 		<Route path='/' component={Main}>
+			<Route path="/home" component={Home} />
 		</Route>
 	</Router>
 );
 
-module.exports routes;
+module.exports = routes;
--- app/index.js
+++ app/index.js
@@ -1,11 +1,6 @@
-var USER_DATA = {
-	name: 'Annie Bae',
-	username: 'anniebae',
-	image: 'https://avatars2.githubusercontent.com/u/8563857?v=3&s=460'
-}
-
 var React = require('react');
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
 
 // acronym to test React Components against (Addy Osmani)
 /*
@@ -16,64 +11,9 @@ var ReactDOM = require('react-dom');
 	Testable
 */
 
-var ProfilePic = React.createClass({
-	render: function () {
-		return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
-	}
-});
-
-var Link = React.createClass({
-	changeURL: function() {
-		window.location.replace(this.props.href)
-	},
-
-	render: function() {
-		return (
-			<span 
-				style={{color:'blue', cursor:'pointer'}}
-				onClick={this.changeURL}>
-				{this.props.children}
-			</span>
-		)
-	}
-});
-
-var ProfileLink = React.createClass({
-	render: function () {
-		return (
-			<div>
-				<Link href={'https://www.github.com/' + this.props.username}>
-					{this.props.username}
-				</Link>
-			</div>
-		)
-	}
-});
-
-var ProfileName = React.createClass({
-	render: function () {
-		return (
-			<div>{this.props.name}</div>
-		)
-	}
-});
-
-// Avatar component encapsulates pic, link,name
-// nesting react components
-var Avatar = React.createClass({
-	render: function() {
-		return (
-			<div>
-				<ProfilePic imageUrl={this.props.user.image} />
-				<ProfileName name={this.props.user.name} />
-				<ProfileLink username={this.props.user.username} />
-			</div>
-		)
-	}
-})
 
 ReactDOM.render(
-	<Avatar user={USER_DATA} />,
+	routes,
 	document.getElementById('app')
 );
 
