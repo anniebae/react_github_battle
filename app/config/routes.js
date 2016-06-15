1dd7a8f705470f5cb72e4a06459ea7f424014d77
Annie Bae <bae.annie@gmail.com>
2016-06-15 16:53:11 -0400

    requiring component that are yet to be built

--- app/config/routes.js
+++ app/config/routes.js
@@ -3,3 +3,6 @@ var ReactRouter = require('react-router');
 var Router 		= ReactRouter.Router;
 var Route 		= ReactRouter.Route;
 var IndexRoute 	= ReactRouter.IndexRoute;
+
+var Main = require('../components/Main');
+var Home = require('../components/Home');
