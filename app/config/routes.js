30c01a08891bf01ec37b4c1731848955edad2ee0
Annie Bae <bae.annie@gmail.com>
2016-06-15 16:55:45 -0400

    creating routes with Main component always active

--- app/config/routes.js
+++ app/config/routes.js
@@ -6,3 +6,12 @@ var IndexRoute 	= ReactRouter.IndexRoute;
 
 var Main = require('../components/Main');
 var Home = require('../components/Home');
+
+var routes = (
+	<Router>
+		<Route path='/' component={Main}>
+		</Route>
+	</Router>
+);
+
+module.exports routes;
