a3235f3b6eff1c5c7a232dc6c59cefb6b5e6f70d
Annie Bae <bae.annie@gmail.com>
2016-06-23 17:17:33 -0400

    debugging to fetch proper github info

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -2,24 +2,24 @@ var axios = require('axios');
 
 var id = "YOUR_CLIENT_ID";
 var sec = "YOUR_SECRET_ID";
-var param = "?client_id" + id + "&client_secret=" + sec;
+var param = "?client_id=" + id + "&client_secret=" + sec;
 
-function getUserInfo(username) {
-	return axios.get('https://api.github.com/users/' + username + param)
+function getUserInfo (username) {
+  return axios.get('https://api.github.com/users/' + username + param);
 }
 
 var helpers = {
-	getPlayersInfo: function(players) {
-		return axios.all(plaers.map(function(username) {
-			return getUserInfo(username)
-		})).then(function (info) {
-			return info.map(function(user) {
-				return user.data;
-			})
-		}).catch(function(err) {
-			console.warn('Error in getPlayersInfo', err);
-		})
-	}
+  getPlayersInfo: function (players) {
+    return axios.all(players.map(function (username) {
+      return getUserInfo(username)
+    }))
+    .then(function (info) {
+      return info.map(function (user) {
+        return user.data
+      })
+    })
+    .catch(function (err) {console.warn('Error in getPlayersInfo: ', err)})
+  }
 };
 
 module.exports = helpers;
