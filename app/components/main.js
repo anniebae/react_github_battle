62d307cae3c3b40b9da705e18d23729ef599640d
Annie Bae <bae.annie@gmail.com>
2016-06-30 13:54:11 -0400

    implementing React CSS transition group

--- app/components/main.js
+++ app/components/main.js
@@ -1,10 +1,17 @@
 var React = require('react');
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+require('../main.css');
 
 var Main = React.createClass({
 	render: function() {
 		return (
 			<div className='main-container'>
-				{this.props.children}
+				<ReactCSSTransitionGroup
+					transitionName="appear"
+					transitionEnterTimeout={500}
+					transitionLeaveTimeout={500}>
+					{this.props.children}
+				</ReactCSSTransitionGroup>
 			</div>
 		)
 	}
