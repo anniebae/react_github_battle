55cdd6c2c209497fb5df14277793873828966045
Annie Bae <bae.annie@gmail.com>
2016-07-21 12:59:02 -0400

    creating separate components for each part of avatar

--- app/index.js
+++ app/index.js
@@ -1,16 +1,51 @@
+var USER_DATA = {
+	name: "Adele",
+	username: "adele",
+	image: 'https://pbs.twimg.com/profile_images/657199367556866048/EBEIl2ol.jpg'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
 	render: function() {
-		console.log(this.props)
+		return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+	}
+});
+
+var ProfileLink = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<a href={'http://adele.com/'}>{this.props.username}</a>
+			</div>
+		)
+	}
+});
+
+var ProfileName = React.createClass({
+	render: function() {
+		return (
+			<div>
+				{this.props.name}
+			</div>
+		)
+	}
+});
+
+var Avatar = React.createClass({
+	render: function() {
 		return(
-			<div> Hello {this.props.name} </div>
+			<div>
+				<ProfilePic imageUrl={this.props.user.image} />
+				<ProfileName name={this.props.user.name} />
+				<ProfileLink username={this.props.user.username} />
+			</div>
 		)
 	}
 });
 
 ReactDOM.render(
-	<HelloWorld name="Annie" anySortOfData={24} />,
+	<Avatar user={USER_DATA} />,
 	document.getElementById('app')
 )
