a12d37a5e4d983303e6be75f193310f28f09ee25
Annie Bae <bae.annie@gmail.com>
2016-07-21 12:50:39 -0400

    pure functions data

--- app/index.js
+++ app/index.js
@@ -3,13 +3,14 @@ var ReactDOM = require('react-dom');
 
 var HelloWorld = React.createClass({
 	render: function() {
+		console.log(this.props)
 		return(
-			<div> Hello Code Genius </div>
+			<div> Hello {this.props.name} </div>
 		)
 	}
 });
 
 ReactDOM.render(
-	<HelloWorld />,
+	<HelloWorld name="Annie" anySortOfData={24} />,
 	document.getElementById('app')
 )
