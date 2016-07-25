dc78c55114ae7052fbae076e674182bb58c0cd2b
Annie Bae <bae.annie@gmail.com>
2016-07-25 11:26:27 -0400

    using props.children and running changeURL function on click Link click

--- app/index.js
+++ app/index.js
@@ -14,10 +14,16 @@ var ProfilePic = React.createClass({
 });
 
 var Link = React.createClass({
+	changeURL: function() {
+		window.location.replace(this.props.href)
+	},
 	render: function() {
 		return (
-			<span>
-				'heyy'
+			<span 
+
+				style={{color: 'blue', cursor: 'pointer'}}
+				onClick={this.changeURL}>
+				{this.props.children}
 			</span>
 		)
 	}
