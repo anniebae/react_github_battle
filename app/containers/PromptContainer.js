c5ceb2b2dd5a3f39ea9a131ee318350c8cc6f7bb
Annie Bae <bae.annie@gmail.com>
2016-07-25 12:57:08 -0400

    download emmett in sublime

---
+++ app/containers/PromptContainer.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var PromptContainer = React.createClass({
+	render: function() {
+		return (
+			<ul>
+				li.person{{<person className="name"></person>}}
+			</ul>
+		)
+	}
+});
+
+
+module.exports = PromptContainer;
