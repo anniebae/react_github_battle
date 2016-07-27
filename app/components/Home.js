eb4d2243b543f22eb8ae01ce38d0f935ba4616e8
Annie Bae <annie.bae@NYC-AnnieBae-rMBP.local>
2016-07-27 17:51:30 -0400

    home component

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,10 +1,12 @@
 var React = require('react');
+var transparentBg = require('../').transparentBg;
 
 var Home = React.createClass({
 	render: function() {
 		return(
-			<div>
-				HOME
+			<div className="jumbotron col-sm-12 text-center" style={transparentBg}>
+				<h1>Github Battle</h1>
+				<p className="lead">Some fancy motto</p>
 			</div>
 		)
 	}
