bb74b9567696fe24658f5774f729a54663f671cf
Annie Bae <annie.bae@wirestone.com>
2016-08-26 15:54:39 -0400

    applying styles by requiring in separate files

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -3,8 +3,7 @@ var transparentBg = require('../styles').transparentBg;
 
 var PromptContainer = React.createClass({
 	render: function() {
-		console.log(this);
-		return (
+		return(
 			<div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
 				<h1>{this.props.route.header}</h1>
 				<div className="col-sm-12">
@@ -13,11 +12,11 @@ var PromptContainer = React.createClass({
 							<input 
 								className="form-control"
 								placeholder="Github Username"
-								type="text" />
+								type="text"/>
 						</div>
 
 						<div className="form-group col-sm-4 col-sm-offset-4">
-							<button 
+							<button
 								className="btn btn-block btn-success"
 								type="submit">
 									Continue
@@ -28,6 +27,6 @@ var PromptContainer = React.createClass({
 			</div>
 		)
 	}
-})
+});
 
 module.exports = PromptContainer;
